828f233e580364fd187bf73f4fe974c9292261cb
taewona <nohteawon@gmail.com>
2022-11-02 14:27:59 +0900

    layout 구현

---
+++ PEER/src/main/java/peer/controller/main/MainController.java
@@ -0,0 +1,14 @@
+package peer.controller.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping(value= {"/","/index","/main"})
+	public String index() {
+		return "index";
+	}
+
+}
