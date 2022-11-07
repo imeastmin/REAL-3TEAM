898dab45d242d66b6fb34f877346fa7393aa9976
taewona <nohteawon@gmail.com>
2022-11-07 18:28:56 +0900

    선택쪽지 삭제 기능(체크박스 체크된 message_num 값 controller로 전달)

--- PEER/src/main/java/peer/message/MessageController.java
+++ PEER/src/main/java/peer/message/MessageController.java
@@ -1,13 +1,23 @@
 package peer.message;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MessageController {
 	 
 	@RequestMapping("/message")
 	public String message() {
+		return "/message/messagebox";
+	}
+	
+	@RequestMapping("/msgDel")
+	public String msgDel(String checklist) {
+		// 체크용 코드
+		System.out.println("넘어와야하는값:"+checklist);
 		return "/message/messagebox";
 	}
 }
