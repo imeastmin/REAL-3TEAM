785a498163a98638bf6d63d3d56034865a67a92b
eastmin <imsenhaku@gmail.com>
2022-11-10 18:05:06 +0900

    어드민 페이지 구현완료 - 김동민

---
+++ PEER/src/main/java/peer/service/admin/AdminServiceImp.java
@@ -0,0 +1,34 @@
+package peer.service.admin;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import peer.dao.admin.AdminMapper;
+import peer.model.member.MemberBean;
+
+@Service
+public class AdminServiceImp implements AdminService {
+	@Autowired
+	private AdminMapper AdminMapper;
+	
+	/* 유저목록 */
+	@Override
+	public List<MemberBean> getUsers() throws Exception {
+		return AdminMapper.getUsers();
+	}
+	
+	/* 계정정지 */
+	@Override
+	public int suspension(int user_num) throws Exception {
+		return AdminMapper.suspension(user_num);
+	}
+
+	/* 계정 활성화 */
+	@Override
+	public int access(int user_num) throws Exception {
+		return AdminMapper.access(user_num);
+	}
+
+}
