5d6e996eec98aca5c877361c7fcb40467ce8bd69
Taewon <107898753+taewona@users.noreply.github.com>
2022-11-16 18:15:18 +0900

    쪽지 기능 구현완료 - 노태원

--- PEER/src/main/java/peer/dao/booking/BookingListDaoImp.java
+++ PEER/src/main/java/peer/dao/booking/BookingListDaoImp.java
@@ -1,3 +1,4 @@
+
 package peer.dao.booking;
 
 import java.util.List;
@@ -29,3 +30,4 @@ public class BookingListDaoImp implements BookingListDao{
 	}
 
 }
+
--- PEER/src/main/java/peer/dao/message/MessageMapper.java
+++ PEER/src/main/java/peer/dao/message/MessageMapper.java
@@ -3,22 +3,27 @@ package peer.dao.message;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import peer.model.message.MessageBean;
 
 @Mapper
 public interface MessageMapper {
 	// 쪽지 리스트
-	public List<MessageBean> msgList(int user_num, int startPage, int endPage) throws Exception;
+	public List<MessageBean> msgList(@Param("user_num") int user_num, 
+			@Param("startPage") int startPage, @Param("endPage") int endPage) throws Exception;
 	
 	// 쪽지 전체 개수
 	public int msgTotal(int user_num) throws Exception;
 	
 	// 쪽지 보내기&답장하기
-	public String msgSend(MessageBean msg) throws Exception;
+	public void msgSend(MessageBean msg) throws Exception;
+	
+	// 회원 권한(상태) 체크
+	public int userCheck(int user_num) throws Exception;
 	
 	// 회원번호에서 닉네임 추출하기
-	public String nicktoUser_num(int message_receiver_num) throws Exception;
+	public String nickFromNum(int message_receiver_num) throws Exception;
 	
 	// 쪽지 상세보기
 	public MessageBean msgOpen(int message_num) throws Exception;
